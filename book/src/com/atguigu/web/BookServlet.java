65554dec11c45ee1189455d96cdf82f1ed82f817
Adil <Adiljan.anwar@gmail.com>
2022-07-27 17:33:46 +0300

    BOOK Store Project

---
+++ book/src/com/atguigu/web/BookServlet.java
@@ -0,0 +1,40 @@
+package com.atguigu.web;
+
+import com.atguigu.pojo.Book;
+import com.atguigu.service.BookService;
+import com.atguigu.service.impl.BookServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class BookServlet extends BaseServlet {
+
+
+    private BookService bookService = new BookServiceImpl();
+    protected void add(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    protected void delete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    protected void update(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    protected void list(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //1 通过BookService查询全部图书
+        List<Book> books = bookService.queryBooks();
+        //2 把全部图书保存到Request域中
+        req.setAttribute("book",books);
+        //3 请求转发到/pages/manager/book_manager.jsp页面
+        req.getRequestDispatcher("/pages/manager/book_manager.jsp").forward(req,resp);
+
+
+    }
+
+}
